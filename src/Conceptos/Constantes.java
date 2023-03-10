e40d1442a9ab5534325c62c339eaabbd1d1284d3
JoseleBarcelona <124534590+JoseleBarcelona@users.noreply.github.com>
2023-03-10 20:36:31 +0100

    Add files via upload

--- src/Conceptos/Constantes.java
+++ src/Conceptos/Constantes.java
@@ -1,2 +1,14 @@
-package Conceptos;public class Constantes {
+package Conceptos;
+
+public class Constantes {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        final int numero2 = 20;
+          numero1 = 15; //esta variable puede sobreescribirse
+        //numero2 = 25; //esta variable no puede sobreescribirse, porque se ha declarado una constante con(final).
+
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+    }
 }
